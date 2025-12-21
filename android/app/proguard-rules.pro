27594620bbbaaaaf3200bd6f49cf91d69fb3ace1
Hoang Nguyen <vhoangbk@gmail.com>
2025-12-21 15:57:43 +0700

    static

--- android/app/proguard-rules.pro
+++
@@ -1,5 +0,0 @@
--keep class com.diff.beeconvert.VideoHelper$* { *; }
-
--keepclassmembers class com.diff.beeconvert.WebServer {
-    public static java.lang.String SAVED_FOLDER;
-}
--- bin/ios-copy.sh
+++
@@ -1,5 +0,0 @@
-rm -rf ios/public
-mkdir -p ios/public
-cp -r public/* ios/public/
-#rm ios/public/libs/ffmpeg-wasm/ffmpeg-st-gpl.js
-#rm ios/public/libs/ffmpeg-wasm/ffmpeg-st-gpl.wasm
